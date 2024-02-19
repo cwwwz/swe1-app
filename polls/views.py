36d4b0f1063a831599c88478d3407514fa7e5b2e
Wanzhao Cheng <wc1609@nyu.edu>
2024-02-18 23:17:09 -0500

    added health check response

--- polls/views.py
+++ polls/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
 from django.views import generic
@@ -46,3 +46,6 @@ def vote(request, question_id):
         # with POST data. This prevents data from being posted twice if a
         # user hits the Back button.
         return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))
+    
+def health_check(request):
+    return HttpResponse("OK", content_type="text/plain")
