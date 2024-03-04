da4efd5c15a07c3ab39ad890045af8476c169067
Wanzhao Cheng <wc1609@nyu.edu>
2024-03-03 21:14:57 -0500

    modified django version

--- ebdjango/settings.py
+++ ebdjango/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = "django-insecure-7i3#az+zhhmbvhucy0mk9lafl61251ylh)==0g^c1e*nlf2s+g
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['wanzhao-hw-env.eba-zd7ptkqp.us-east-1.elasticbeanstalk.com']
+ALLOWED_HOSTS = ['wanzhao-hw-env.eba-zd7ptkqp.us-east-1.elasticbeanstalk.com', '127.0.0.1']
 
 
 # Application definition
